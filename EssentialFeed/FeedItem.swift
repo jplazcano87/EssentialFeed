d5a66d1cca6634131ba130b5d78d943649b84849
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-22 15:51:21 -0300

    Added Project Structure

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Juan Pablo Lazcano Candia on 22-12-21.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
