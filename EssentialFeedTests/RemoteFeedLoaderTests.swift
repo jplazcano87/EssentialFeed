2ce3ecfb418e41c3687188d613b92b0633c8ed7a
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-26 18:00:10 -0300

    Replace Optional with enum

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -89,7 +89,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   
   private class HTTPClientSpy: HTTPClient {
     //array of tuple to capture both urls and completion
-    private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
     // computed properties that fills with only the urls from the tuples array
     var requestedURLs: [URL] {
       return messages.map {
@@ -97,12 +97,12 @@ class RemoteFeedLoaderTests: XCTestCase {
       }
     }
     
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
       messages.append((url, completion))
     }
     
     func complete(with error: Error, at index: Int = 0) {
-      messages[index].completion(error, nil)
+      messages[index].completion(.failure(error))
     }
     
     func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -111,8 +111,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         statusCode: code,
         httpVersion: nil,
         headerFields: nil
-      )
-      messages[index].completion(nil, response)
+      )!
+      messages[index].completion(.success(response))
       
     }
   }
