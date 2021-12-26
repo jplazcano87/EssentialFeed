b3f3e9fab7faa8e47574b8ae147bc832b363e4fe
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-26 17:32:42 -0300

    delivers invalid data on httpresponse

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -27,7 +27,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     XCTAssertEqual(client.requestedURLs, [url])
   }
-
+  
   
   func test_loadTwice_requestsDataFromURLTwice() {
     let url = URL(string: "https://a-given-url.com")!
@@ -36,33 +36,44 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     sut.load { _ in }
     sut.load { _ in }
-   
+    
     XCTAssertEqual(client.requestedURLs, [url, url])
     
   }
- /*
-  This test is using a property inyection into the client
+  /*
+   This test is using a property inyection into the client
+   func test_load_deliversErrorOnClientError() {
+   let (sut, client) = makeSUT()
+   client.error = NSError(domain: "A Error", code: 0)
+   //create an array of error, because we can compare the type and the number of times that specific error ocurrs
+   var capturedErrors = [RemoteFeedLoader.Error]() // this array is for the capture
+   sut.load { capturedErrors.append($0) } // Appends from the closure
+   XCTAssertEqual(capturedErrors, [.conectivity])
+   }
+   */
+  
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
-    client.error = NSError(domain: "A Error", code: 0)
     //create an array of error, because we can compare the type and the number of times that specific error ocurrs
     var capturedErrors = [RemoteFeedLoader.Error]() // this array is for the capture
     sut.load { capturedErrors.append($0) } // Appends from the closure
-    XCTAssertEqual(capturedErrors, [.conectivity])
+    
+    client.complete(withStatusCode: 400)
+    XCTAssertEqual(capturedErrors, [.invalidData])
   }
-  */
   
-   func test_load_deliversErrorOnClientError() {
+  func test_load_deliversErrorOnNo200HTTPResponse() {
     let (sut, client) = makeSUT()
     //create an array of error, because we can compare the type and the number of times that specific error ocurrs
     var capturedErrors = [RemoteFeedLoader.Error]() // this array is for the capture
     sut.load { capturedErrors.append($0) } // Appends from the closure
     
     let clientError = NSError(domain: "A Error", code: 0)
-     
+    
     client.complete(with: clientError)
     XCTAssertEqual(capturedErrors, [.conectivity])
   }
+  
   //Helpers
   private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
     let client = HTTPClientSpy()
@@ -71,21 +82,32 @@ class RemoteFeedLoaderTests: XCTestCase {
   }
   
   private class HTTPClientSpy: HTTPClient {
-   //array of tuple to capture both urls and completion
-    private var messages = [(url: URL, completion: (Error) -> Void)]()
-   // computed properties that fills with only the urls from the tuples array
+    //array of tuple to capture both urls and completion
+    private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+    // computed properties that fills with only the urls from the tuples array
     var requestedURLs: [URL] {
       return messages.map {
         $0.url
       }
     }
     
-    func get(from url: URL, completion: @escaping (Error) -> Void) {
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
       messages.append((url, completion))
     }
     
     func complete(with error: Error, at index: Int = 0) {
-      messages[index].completion(error)
+      messages[index].completion(error, nil)
+    }
+    
+    func complete(withStatusCode code: Int, at index: Int = 0) {
+      let response = HTTPURLResponse(
+        url: requestedURLs[index],
+        statusCode: code,
+        httpVersion: nil,
+        headerFields: nil
+      )
+      messages[index].completion(nil, response)
+      
     }
   }
   
