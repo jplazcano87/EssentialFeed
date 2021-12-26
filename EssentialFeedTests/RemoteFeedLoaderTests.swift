393e24b4e034c96926e1bf983e48132456a9e70f
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-26 16:54:13 -0300

    replace stubbing by capture values

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -40,16 +40,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     XCTAssertEqual(client.requestedURLs, [url, url])
     
   }
-  
+ /*
+  This test is using a property inyection into the client
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
-    client.error = NSError(domain: "A Error", code: 0) // property inyection to the client
+    client.error = NSError(domain: "A Error", code: 0)
     //create an array of error, because we can compare the type and the number of times that specific error ocurrs
     var capturedErrors = [RemoteFeedLoader.Error]() // this array is for the capture
     sut.load { capturedErrors.append($0) } // Appends from the closure
     XCTAssertEqual(capturedErrors, [.conectivity])
   }
+  */
   
+   func test_load_deliversErrorOnClientError() {
+    let (sut, client) = makeSUT()
+    //create an array of error, because we can compare the type and the number of times that specific error ocurrs
+    var capturedErrors = [RemoteFeedLoader.Error]() // this array is for the capture
+    sut.load { capturedErrors.append($0) } // Appends from the closure
+    
+    let clientError = NSError(domain: "A Error", code: 0)
+     client.completions[0](clientError)
+     
+    XCTAssertEqual(capturedErrors, [.conectivity])
+  }
   //Helpers
   private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
     let client = HTTPClientSpy()
@@ -60,10 +73,9 @@ class RemoteFeedLoaderTests: XCTestCase {
   private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
     var error: Error?
+    var completions = [(Error) -> Void]()
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-      if let error = error {
-        completion(error)
-      }
+      completions.append(completion)
       requestedURLs.append(url)
     }
     
