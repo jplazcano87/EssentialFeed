1bf28dffe348d9066c842b73caa19ce41d2d2bdb
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-26 22:20:49 -0300

    decouple feeditem from api details

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -41,7 +41,9 @@ public final class RemoteFeedLoader {
       switch result {
       case let .success(data, response):
         if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-          completion(.sucesss(root.items))
+          completion(.sucesss(root.items.map({
+            $0.item
+          })))
         } else {
           completion(.failure(.invalidData))
         }
@@ -54,7 +56,16 @@ public final class RemoteFeedLoader {
 }
 
 private struct Root: Decodable {
-  let items: [FeedItem]
+  let items: [Item]
 }
 
-
+private struct Item: Decodable {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let image: URL
+  
+  var item: FeedItem {
+    return FeedItem(id: id, description: description, location: location, imageURL: image)
+  }
+}
