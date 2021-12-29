0eec9b572133daba068d8610743ebde95989cb1b
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-29 19:14:02 -0300

    added expectation and switch to remove equatable from protocol

--- EssentialFeed/Feed API/FeedItemMapper.swift
+++ EssentialFeed/Feed API/FeedItemMapper.swift
@@ -7,15 +7,14 @@
 
 import Foundation
 
-internal final class FeedItemMapper {
-  
+internal final class FeedItemsMapper {
   private struct Root: Decodable {
     let items: [Item]
+    
     var feed: [FeedItem] {
-      items.map { $0.item }
+      return items.map { $0.item }
     }
   }
- 
   
   private struct Item: Decodable {
     let id: UUID
@@ -28,17 +27,14 @@ internal final class FeedItemMapper {
     }
   }
   
-  private static var OK_200: Int {
-    return 200
-  }
+  private static var OK_200: Int { return 200 }
   
   internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
     guard response.statusCode == OK_200,
-          let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(.invalidData)
-          }
-    
+      let root = try? JSONDecoder().decode(Root.self, from: data) else {
+      return .failure(RemoteFeedLoader.Error.invalidData)
+    }
+
     return .success(root.feed)
   }
-  
 }
--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,33 +7,32 @@
 
 import Foundation
 
-public final class RemoteFeedLoader {
-  
+public final class RemoteFeedLoader: FeedLoader {
   private let url: URL
   private let client: HTTPClient
   
   public enum Error: Swift.Error {
-    case conectivity
+    case connectivity
     case invalidData
   }
   
-  public typealias Result = LoadFeedResult<Error>
+  public typealias Result = LoadFeedResult
   
   public init(url: URL, client: HTTPClient) {
-    self.client = client
     self.url = url
+    self.client = client
   }
   
   public func load(completion: @escaping (Result) -> Void) {
     client.get(from: url) { [weak self] result in
       guard self != nil else { return }
+      
       switch result {
       case let .success(data, response):
-        completion(FeedItemMapper.map(data, from: response))
+        completion(FeedItemsMapper.map(data, from: response))
       case .failure:
-        completion(.failure(.conectivity))
+        completion(.failure(Error.connectivity))
       }
     }
   }
-  
 }
