0e76c6fc0307b9dc255a4f16c29b796d95378ad8
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2021-12-29 20:50:16 -0300

    Added Test for the URL Session client, first case check if the url is correct

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,49 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by Juan Pablo Lazcano Candia on 29-12-21.
+//
+
+import XCTest
+
+
+class URLSessionHTTPClient {
+  private let session: URLSession
+  
+  init(session: URLSession) {
+    self.session = session
+  }
+  
+  func get(from url: URL) {
+    session.dataTask(with: url) { _, _, _ in }
+  }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+  
+  func test_getFromURL_createsDataTaskWithURL() {
+    let url = URL(string: "http://any-url.com")!
+    let session = URLSessionSpy()
+    let sut = URLSessionHTTPClient(session: session)
+    
+    sut.get(from: url)
+    XCTAssertEqual(session.receivedURLs, [url])
+  }
+  
+  // MARK: - Helpers
+  private class URLSessionSpy: URLSession {
+    var receivedURLs = [URL]()
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+      receivedURLs.append(url)
+      return FakeURLSessionDataTask()
+    }
+  }
+  
+  // a fake or mock does nothing
+  private class FakeURLSessionDataTask: URLSessionDataTask {
+    
+  }
+  
+}
