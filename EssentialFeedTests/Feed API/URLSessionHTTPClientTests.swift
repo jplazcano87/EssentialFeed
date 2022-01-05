bcbd4469b304eb3796d104351ee4eb8b5fe5ba91
Juan Lazcano Candia <jp.lazcano87@gmail.com>
2022-01-04 21:27:56 -0300

    move the httpclient to production code

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -1,28 +1,6 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPClient {
-  private let session: URLSession
-  
-  init(session: URLSession = .shared) {
-    self.session = session
-  }
-  
-  struct UnexpectedValuesRepresentation: Error {}
-  
-  func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-    session.dataTask(with: url) { data, response, error in
-      if let error = error {
-        completion(.failure(error))
-      } else if let data = data, let response = response as? HTTPURLResponse{
-        completion(.success(data, response))
-      } else {
-        completion(.failure(UnexpectedValuesRepresentation()))
-      }
-    }.resume()
-  }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
   
   override func setUp() {
@@ -91,7 +69,7 @@ class URLSessionHTTPClientTests: XCTestCase {
   
   // MARK: - Helpers
   
-  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
+  private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
     let sut = URLSessionHTTPClient()
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
